e7c01fa654345ba93c59b08863b2c789ca9f3d7b
AndrewSergienko <sergienkoandrew9922@gmail.com>
2022-03-29 10:16:17 +0300

    add chats models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from account.models import CustomUser
 
-# Create your models here.
+
+class Chat(models.Model):
+    users = models.ManyToManyField(CustomUser, related_name='chats')
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    user = models.ForeignKey(CustomUser, null=True, on_delete=models.SET_NULL, related_name='messages')
+    text = models.TextField(default='')
+    date = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
