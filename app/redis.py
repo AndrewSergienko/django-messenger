507012864818f046532e11f8f170dae3bbce0b45
AndrewSergienko <sergienkoandrew9922@gmail.com>
2022-03-31 19:29:29 +0300

    using redis instead of db for chats

---
+++ app/redis.py
@@ -0,0 +1,7 @@
+import redis
+from django.conf import settings
+
+redis = redis.Redis(host=settings.REDIS_HOST,
+                    port=settings.REDIS_PORT,
+                    db=settings.REDIS_DB,
+                    decode_responses=True)
--- chat/consumers.py
+++ chat/consumers.py
@@ -2,32 +2,32 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 
-from .models import Chat, ChannelUser
 from account.models import CustomUser
+from app.redis import redis
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         # Створює поле [user, channel_name] для подальшої ідентифікації
-        await self.create_channel_user()
+        redis.set(str(self.scope['user'].id), self.channel_name)
         await self.accept()
 
     async def disconnect(self, code):
-        await self.delete_channel_user()
+        redis.delete(str(self.scope['user'].id))
+        # await self.delete_channel_user()
 
     async def receive(self, text_data=None, bytes_data=None):
         try:
             text_data_json = json.loads(text_data)
-            user = await self.get_channel_user(text_data_json['user_id'])
-            user_channel_name = await self.get_channel_name(user)
+            channel_name = redis.get(text_data_json['user_id'])
             message = text_data_json['message']
-            await self.channel_layer.send(user_channel_name,
+            await self.channel_layer.send(channel_name,
                                           {
                                               'type': 'chat_message',
                                               'message': message
                                           })
         # Якщо не зловить помилку, то це приведе до websocket disconect
-        except (CustomUser.DoesNotExist, ChannelUser.DoesNotExist):
+        except CustomUser.DoesNotExist:
             pass
 
     async def chat_message(self, event):
@@ -41,17 +41,5 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Django ORM не підтримує асинхронність, тому потрібно використовувати декоратори
     @database_sync_to_async
-    def create_channel_user(self):
-        ChannelUser.objects.get_or_create(user=self.scope['user'], channel_name=self.channel_name)
-
-    @database_sync_to_async
-    def delete_channel_user(self):
-        ChannelUser.objects.filter(channel_name=self.channel_name).delete()
-
-    @database_sync_to_async
-    def get_channel_user(self, user_id):
+    def get_user(self, user_id):
         return CustomUser.objects.get(id=user_id)
-
-    @database_sync_to_async
-    def get_channel_name(self, user):
-        return user.channel_name.channel_name
