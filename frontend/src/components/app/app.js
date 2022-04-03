b7ccf07b0a60ae953194cd6137cb479dc0e39d1d
Andrey Slynko <qkston22@gmail.com>
2022-04-03 19:56:01 +0300

    start making the login page

--- frontend/src/components/app/app.js
+++ frontend/src/components/app/app.js
@@ -3,28 +3,47 @@ import Login from '../login';
 
 
 export default class App extends Component {
-   componentDidMount() {
-       let url = 'http://127.0.0.1:8000/api/auth/',
+   state = {
+      authToken: ""
+   }
+   
+   async componentDidMount() {
+      const token = await this.getToken();
+
+      this.setState({authToken: token})
+   }
+
+   async getToken() {
+      let url = 'http://127.0.0.1:8000/api/auth/',
           data = {username: "testuser1", password: "abobaaboba"};
 
-       fetch(url, {
-             method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-               'Content-Type': 'application/json'
-             }
-          })
-          .then(response => response.json())
-          .then(myJSON => console.log('Success: ', myJSON))}
+      const auth = await fetch(url, {
+         method: 'POST',
+         body: JSON.stringify(data),
+         headers: {
+            'Content-Type': 'application/json'
+         }
+      })
+      .then(response => response.json());
+
+      return auth.token;
+   }
       
    render() {
-       const result = fetch("http://127.0.0.1:8000/api/chats/")
-         .then(res => res.json())
-         .then(res => console.log(res))
+      // const myHeaders = new Headers();
+      // myHeaders.append('Content-Type', 'application/json');
+      // myHeaders.append('Authorization', `Token ${this.state.authToken}`);
+
+      // const result = fetch("http://127.0.0.1:8000/api/chats/", {
+      //    method: "GET",
+      //    headers: myHeaders
+      // })
+      //    .then(res => res.json())
+      //    .then(res => console.log(res))
 
       return (
          <div className="App">
-             {/*{result}*/}
+            {/* {result} */}
             <Login/>
          </div>
       );
--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
+import './login.css';
+
 export default class Login extends Component {
    render() {
       return (
          <>
-            <p>Hello world</p>
+            <form className='form d-flex align-items-center' action='http://127.0.0.1:8000/account/login' method='post'>
+               <h2>Login</h2>
+               <div className="form-group login">
+                  <label htmlFor="login-input">Username</label>
+                  <input type="text" className="form-control" id="login-input" placeholder="Enter username"/>
+               </div>
+               <div className="form-group password">
+                  <label htmlFor="password-input">Password</label>
+                  <input type="password" className="form-control" id="password-input" placeholder="Enter password"/>
+               </div>
+               <button type="submit" className="btn btn-primary btn-for-submit">Login</button>
+            </form>
          </>
       )
    }
