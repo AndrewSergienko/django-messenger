48ad7b83b043c90830474a7567624ce760477c63
Andrey Slynko <qkston22@gmail.com>
2022-04-06 13:45:53 +0300

    styled login page and made initial logic with server

--- frontend/src/components/app/app.js
+++ frontend/src/components/app/app.js
@@ -4,18 +4,16 @@ import Login from '../login';
 
 export default class App extends Component {
    state = {
-      authToken: ""
-   }
-   
-   async componentDidMount() {
-      const token = await this.getToken();
-
-      this.setState({authToken: token})
+      authToken: "",
+      username: "",
+      password: "",
+      error: false
    }
 
-   async getToken() {
+   // Get token for authorization
+   getToken = async (username, password) => {
       let url = 'http://127.0.0.1:8000/api/auth/',
-          data = {username: "testuser1", password: "abobaaboba"};
+          data = {username, password};
 
       const auth = await fetch(url, {
          method: 'POST',
@@ -23,28 +21,43 @@ export default class App extends Component {
          headers: {
             'Content-Type': 'application/json'
          }
-      })
-      .then(response => response.json());
+      });
 
-      return auth.token;
+      // if don't get show error message
+      if (!auth.ok) {
+         this.setState({error: true});
+      } else {
+         return auth.json();
+      }
+   }
+
+   // Get username and password from login form
+   getDataFromForm = async (username, password) => {
+      // Send request
+      const auth = await this.getToken(username, password);
+
+      if (auth) {
+         this.setState({authToken: auth.token})
+         console.log(this.state.authToken);
+      }
    }
       
    render() {
-       const myHeaders = new Headers();
-       myHeaders.append('Content-Type', 'application/json');
-       myHeaders.append('Authorization', `Token ${this.state.authToken}`);
+      //  const myHeaders = new Headers();
+      //  myHeaders.append('Content-Type', 'application/json');
+      //  myHeaders.append('Authorization', `Token ${this.state.authToken}`);
 
-       const result = fetch("http://127.0.0.1:8000/api/chats/", {
-          method: "GET",
-          headers: myHeaders
-       })
-          .then(res => res.json())
-          .then(res => console.log(res))
+      //  const result = fetch("http://127.0.0.1:8000/api/chats/", {
+      //     method: "GET",
+      //     headers: myHeaders
+      //  })
+      //     .then(res => res.json())
+      //     .then(res => console.log(res))
 
       return (
          <div className="App">
             {/* {result} */}
-            <Login/>
+            <Login getDataFromForm={this.getDataFromForm} error={this.state.error}/>
          </div>
       );
    }
--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -3,20 +3,62 @@ import React, { Component } from 'react';
 import './login.css';
 
 export default class Login extends Component {
+   state = {
+      username: "",
+      password: ""
+   }
+
+   // Write entered data in inputs to state
+   changeInput = (event) => {
+      const target = event.target;
+      
+      if (target.id === "login-input") {
+         this.setState({username: target.value});
+      } else if (target.id === "password-input") {
+         this.setState({password: target.value});
+      }
+   }
+
+   // When form is submit
+   submitForm = (event) => {
+      event.preventDefault();
+
+      this.props.getDataFromForm(this.state.username, this.state.password);
+   }
+
    render() {
+      const { error } = this.props,
+            errorMessage = error ? "Incorrect data" : null;
+
       return (
          <>
-            <form className='form d-flex align-items-center' action='http://127.0.0.1:8000/account/login' method='post'>
+            <form 
+               className='form d-flex align-items-center'
+               onSubmit={this.submitForm}>
                <h2>Login</h2>
+               <small className='info-message'>Not a member? You can register here</small>
                <div className="form-group login">
                   <label htmlFor="login-input">Username</label>
-                  <input type="text" className="form-control" id="login-input" placeholder="Enter username"/>
+                  <input 
+                     type="text" 
+                     className="form-control" 
+                     id="login-input" 
+                     placeholder="Enter username" 
+                     onChange={this.changeInput}/>
                </div>
                <div className="form-group password">
                   <label htmlFor="password-input">Password</label>
-                  <input type="password" className="form-control" id="password-input" placeholder="Enter password"/>
+                  <input 
+                     type="password" 
+                     className="form-control" 
+                     id="password-input" 
+                     placeholder="Enter password" 
+                     onChange={this.changeInput}/>
                </div>
-               <button type="submit" className="btn btn-primary btn-for-submit">Login</button>
+               <button 
+                  type="submit" 
+                  className="btn btn-primary btn-for-submit">Login</button>
+               <small className='error-message'>{errorMessage}</small>
             </form>
          </>
       )
