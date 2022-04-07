2370287167a6b45fa948165721fa165e5aa24ae3
AndrewSergienko <sergienkoandrew9922@gmail.com>
2022-04-07 13:47:09 +0300

    change CustomUser model

--- account/models.py
+++ account/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    phone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=30, blank=True, unique=True)
+    phone = models.CharField(max_length=20, blank=True, unique=True)
