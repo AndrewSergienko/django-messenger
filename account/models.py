654373fbb4086709845af6bdbec8487ce519a710
AndrewSergienko <sergienkoandrew9922@gmail.com>
2022-05-22 18:16:27 +0300

    update user model

--- account/models.py
+++ account/models.py
@@ -7,4 +7,4 @@ class CustomUser(AbstractUser):
     email_validator = validators.EmailValidator()
     email = models.EmailField(max_length=100, unique=True, validators=[email_validator])
     username = models.CharField(max_length=100, blank=True, unique=True, validators=[AbstractUser.username_validator])
-    phone = models.CharField(max_length=20, blank=True, unique=True)
+    phone = models.CharField(max_length=20, blank=True, unique=True, null=True)
