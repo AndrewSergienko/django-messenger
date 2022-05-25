7011bd6ff7df5211d2480a93d02daa5ccedf5ee0
AndrewSergienko <sergienkoandrew9922@gmail.com>
2022-05-25 21:39:47 +0300

    base chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -16,119 +16,55 @@ class ChatConsumer(AsyncWebsocketConsumer):
         user_id = await self.get_user_id_by_token(token)
         self.scope['user_id'] = user_id
         redis.set(user_id, self.channel_name)
-
-        await self.send_user_active_status('online')
-
         await self.accept()
 
     async def disconnect(self, code):
-        await self.send_user_active_status('offline')
-        redis.delete(self.scope['user_id'])
+        redis.delete(self.scope["url_route"]["kwargs"]["token"])
+        # await self.delete_channel_user()
 
     async def receive(self, text_data=None, bytes_data=None):
-        """
-        Метод викливається з клієнту користувача.
-        """
-        text_data_json = json.loads(text_data)
-        if text_data_json['type'] in ['create_chat_event']:
-            await self.send_create_chat_event(text_data_json)
-        else:
-            text_data_json['chat'] = await self.get_chat_by_id(text_data_json['chat'])
-            text_data_json['user'] = await self.get_user(self.scope['user_id'])
-            data_functions = {
-                'message': self.data_message,
-                'read_event': self.data_read_event,
-            }
-            data = await data_functions[text_data_json['type']](text_data_json)
-            receivers = await self.get_receivers_in_chat(text_data_json['chat'])
-            await self.send_to_receivers(receivers, data)
+        """ Метод викливається з клієнту користувача """
+        try:
+            text_data_json = json.loads(text_data)
+            if text_data_json['type'] == 'message':
+                message_data = {
+                    'chat': text_data_json['chat'],
+                    'user': self.scope['user_id'],
+                    'text': text_data_json['text']
+                }
+                message = await self.create_message(message_data, save=True, return_obj=True)
 
-    """
-    Data defs:
-    data функції обробляють відповідну подію та формують і повертають дані, які потрібно відправити отримувачу 
-    """
-    async def data_message(self, data):
-        message_data = {
-            'chat': data['chat'],
-            'user': data['user'],
-            'text': data['text']
-        }
-        message = await self.create_message(message_data, save=True, return_obj=True)
-        return {
-            'type': 'chat_message',
-            'message_id': message.id
-        }
-
-    async def data_read_event(self, data):
-        message = await self.get_message(data['message_id'])
-        message.read.add(data['user'])
-        await self.update_obj(message)
-        return {
-            'type': 'read_event',
-            'message': message.id,
-            'chat': data['chat'].id,
-            'user': self.scope['user_id']
-        }
-
-    async def send_create_chat_event(self, data):
-        chat = await self.get_chat_by_id(data['chat_id'])
-        receivers = await self.get_receivers_in_chat(chat)
-        data = {
-            'type': 'create_chat_event',
-            'chat_id': data['chat_id'],
-            'chat_type': chat.type,
-        }
-        if chat.type == 'personal':
-            friend_id = receivers[0]
-            data['friend'] = friend_id
-        await self.send_to_receivers(receivers, data)
-
-    async def send_user_active_status(self, status):
-        """
-        Повідомляє друзів про змінення статусу активності користучава.
-        Під "друзями" маються на увазі користувачі, в яких є персональний чат з поточним користувачем
-        TODO: Повідомляти також користувача, який на даний момент переглядає профіль
-        """
-        user = await self.get_user(self.scope['user_id'])
-        personal_chats = await self.get_chats_by_user(user, 'personal')
-        receivers = []
-        for chat in personal_chats:
-            receivers += await self.get_receivers_in_chat(chat)
-        data = {
-            'type': 'user_active_status_event',
-            'user_id': self.scope['user_id'],
-            'status': status
-        }
-        await self.send_to_receivers(receivers, data)
-
-    async def send_to_receivers(self, receivers, data):
-        for receiver in receivers:
-            channel_name = redis.get(str(receiver))
-            if channel_name:
-                await self.channel_layer.send(channel_name, data)
+                chat = message_data['chat']
+                receivers = await self.get_receivers(chat)
+                for receiver in receivers:
+                    channel_name = redis.get(str(receiver))
+                    if channel_name:
+                        await self.channel_layer.send(channel_name,
+                                                      {
+                                                          'type': 'chat_message',
+                                                          'message_id': message.id
+                                                      })
+            elif text_data_json['type'] == 'read':
+                message = await self.get_message(text_data_json['message_id'])
+                message.read = True
+                self.save_obj(message)
+        # Якщо не зловить помилку, то це приведе до websocket disconect
+        except CustomUser.DoesNotExist:
+            pass
 
     async def chat_message(self, event):
         # Метод викликається, якщо ловить повідомлення від іншого користувача
-        message = await self.get_message(event['message_id'], relation='user', serialize=True)
+        message = await self.get_message(event['message_id'], serialize=True)
         message_return = {'type': 'message',
                           'message': message}
 
         # Надсилається повідомлення до JS клієнта в браузері
         await self.send(text_data=json.dumps(message_return))
 
-    async def read_event(self, event):
-        await self.send(text_data=json.dumps(event))
-
-    async def user_active_status_event(self, event):
-        await self.send(text_data=json.dumps(event))
-
-    async def create_chat_event(self, event):
-        await self.send(text_data=json.dumps(event))
-
     # Django ORM не підтримує асинхронність, тому потрібно використовувати декоратори
     @database_sync_to_async
     def get_user_id_by_token(self, key):
-        return Token.objects.values_list('user_id', flat=True).get(key=key)
+        return Token.objects.get(key=key).user_id
 
     @database_sync_to_async
     def get_user(self, pk, serialize=False):
@@ -139,25 +75,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         return user
 
     @database_sync_to_async
-    def get_message(self, pk, relation=False, serialize=False):
-        if relation:
-            message = Message.objects.select_related(relation).get(id=pk)
-        else:
-            message = Message.objects.get(id=pk)
+    def get_message(self, pk, serialize=False):
+        message = Message.objects.get(id=pk)
         if serialize:
-            message_serializer = MessageSerializer(message)
-            result_data = message_serializer.data
-            if relation:
-                if relation == 'chat':
-                    relation_serializer = ChatSerializer(message.chat)
-                else:
-                    relation_serializer = UserSeralizer(message.user)
-                result_data[relation] = relation_serializer.data
-            return result_data
+            serializer = MessageSerializer(message)
+            return serializer.data
         return message
 
     @database_sync_to_async
-    def get_chat_by_id(self, pk, serialize=False):
+    def get_chat(self, pk, serialize=False):
         chat = Chat.objects.get(id=pk)
         if serialize:
             serializer = ChatSerializer(chat)
@@ -165,14 +91,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         return chat
 
     @database_sync_to_async
-    def get_chats_by_user(self, user:CustomUser, chat_type):
-        chats = user.chats.filter(type=chat_type)
-        # якщо залишити в QuerySet, то при спробі отримати елемент, буде виконаний запрос в базу
-        # така спроба є в методі, який не обернутий декоратором database_sync_to_async, тому буде помилка
-        return list(chats)
-
-    @database_sync_to_async
     def create_message(self, data, save=False, return_obj=False):
+        data['chat'] = Chat.objects.get(id=data['chat'])
+        data['user'] = CustomUser.objects.get(id=self.scope['user_id'])
         message = Message(**data)
         if save:
             message.save()
@@ -184,10 +105,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         obj.save()
 
     @database_sync_to_async
-    def update_obj(self, obj, **kwargs):
-        obj.update(**kwargs)
-
-    @database_sync_to_async
-    def get_receivers_in_chat(self, chat: Chat):
-        receivers = chat.users.values_list('id', flat=True).exclude(id=self.scope['user_id'])
-        return list(receivers)
+    def get_receivers(self, chat):
+        users = [user.id for user in chat.users.all()]
+        users.remove(self.scope['user_id'])
+        return users
--- chat/views.py
+++ chat/views.py
@@ -1,58 +1,36 @@
-from account.serializers import UserSeralizer
-from .serializers import ChatSerializer, MessageSerializer
+from .serializers import ChatSerializer
 from .models import Chat, Message
 from account.models import CustomUser
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from app.redis import redis
 
 
 class ChatList(APIView):
     def get(self, request, format=None):
         chats = request.user.chats.all()
         serializer = ChatSerializer(chats, many=True)
-        for i, chat in enumerate(serializer.data):
+        for chat in serializer.data:
             if chat['type'] == 'personal':
-                friend_id = chat['users'][0] if chat['users'][1] == request.user.id else chat['users'][1]
-                friend = CustomUser.objects.get(id=friend_id)
-                friend_serializer = UserSeralizer(friend)
-                chat['friend'] = friend_serializer.data
-                is_online = redis.get(str(friend.id))
-                chat['friend']['active_status'] = "online" if is_online else "offline"
-                message_serializer = MessageSerializer(chats[i].messages.last())
-                chat['last_message'] = message_serializer.data
+                chat['friend'] = chat['users'][0] if chat['users'][1] == request.user.id else chat['users'][1]
                 del chat['users']
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ChatCreate(APIView):
     def post(self, request, format=None):
-        if request.data['user_id']:
-            try:
-                user = CustomUser.objects.get(id=request.data['user_id'])
-                if user == request.user:
-                    return Response({'user_id': 'same user'}, status=status.HTTP_400_BAD_REQUEST)
-                if Chat.objects.filter(users=request.user).filter(users=user):
-                    return Response({'chat': 'chat exist'}, status=status.HTTP_400_BAD_REQUEST)
-                serializer = ChatSerializer(data=request.data)
-                serializer.is_valid(raise_exception=True)
-                chat = serializer.save()
-                chat.users.add(request.user, user)
-                return Response({'id': serializer.data['id']}, status=status.HTTP_201_CREATED)
-            except CustomUser.DoesNotExist:
-                return Response({'user_id': 'not exist'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ChatRemove(APIView):
-    def post(self, request, format=None):
-        chat = get_object_or_404(Chat, id=request.data['chat_id'])
-        if request.user in chat.users.all():
-            chat.delete()
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response({'reason': 'has no right'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = ChatSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            if request.data['user_id']:
+                try:
+                    user = CustomUser.objects.get(id=request.data['user_id'])
+                    chat = serializer.save()
+                    chat.users.add(request.user, user)
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+                except CustomUser.DoesNotExist:
+                    return Response({'user_id': 'not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(data=status.HTTP_400_BAD_REQUEST)
 
 
 class ChatDetail(APIView):
@@ -63,20 +41,5 @@ class ChatDetail(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class Messages(APIView):
-    def get(self, request, chat_pk):
-        chat = get_object_or_404(Chat, id=chat_pk)
-        message = get_object_or_404(Message, id=request.GET['message_id'])\
-            if 'message_id' in request.GET else None
-        direction = request.GET['direction'] if 'direction' in request.GET else None
-        messages_num = int(request.GET['messages_num'])
-        if message and direction:
-            if direction == 'up':
-                messages = chat.messages.filter(id__lt=message.id).order_by('-id')[:messages_num]
-            elif direction == 'down':
-                messages = chat.messages.filter(id__gt=message.id).order_by('-id')[:messages_num]
-        else:
-            messages = chat.messages.order_by('-id')[:messages_num]
-
-        serializer = MessageSerializer(messages, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+def room(request):
+    return render(request, 'chat/room.html')
