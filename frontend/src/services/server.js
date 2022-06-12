f07b3f01f0140a6a3f882d049e09997a7f9f7600
Andrey Slynko <qkston22@gmail.com>
2022-06-12 17:16:38 +0300

    added feature: output all user's chats in sidebar

--- frontend/src/services/server.js
+++ frontend/src/services/server.js
@@ -13,35 +13,45 @@ export default class Server {
       })
    };
 
+   async getResource(url, headers) {
+      return await fetch(`${this._apiBase}${url}`, {headers: headers});
+   };
+
    // Get token for login
-   loginUser = async (email, password) => {
+   loginUser = (email, password) => {
       const data = {email, password};
-      return await this
-                     .postResource('/auth/', data)
-                     .then(errors => errors.json());
+      return this
+               .postResource('/auth/', data)
+               .then(errors => errors.json());
    }
 
    // Registration user
-   createToken = async (email) => {
-      return await this
-                     .postResource('/users/create_token', {email})
-                     .then(errors => errors.json())
-                     .catch(() => {return});
+   createToken = (email) => {
+      return this
+               .postResource('/users/create_token', {email})
+               .then(errors => errors.json())
+               .catch(() => {return});
    }
 
-   verifyToken = async (email, token) => {
+   verifyToken = (email, token) => {
       const data = {email, token};
-      return await this
-                     .postResource('/users/verify_token', data)
-                     .then(errors => errors)
-                     .catch(() => {return});
+      return this
+               .postResource('/users/verify_token', data)
+               .then(errors => errors)
+               .catch(() => {return});
    }
 
-   registrationUser = async (email, username, password, first_name, last_name) => {
+   registrationUser = (email, username, password, first_name, last_name) => {
       const data = {email, username, password, first_name, last_name};
-      return await this
-                     .postResource('/users/', data)
-                     .then(errors => errors.json())
-                     .catch(() => {return});
+      return this
+               .postResource('/users/', data)
+               .then(errors => errors.json())
+               .catch(() => {return});
+   }
+
+   getChatsList = (token) => {
+      return this
+               .getResource(`/chats/list?token=${token}`, {'Authorization': `Token ${token}`})
+               .then(chats => chats.json())
    }
 }
