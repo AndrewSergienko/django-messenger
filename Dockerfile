67ba8bffa47e4da6ca4f3882a4dfa5d9fafa5ee9
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2022-06-14 17:12:48 +0300

    update settings

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ ENV PYTHONUNBUFFERED 1
 WORKDIR /app/api
 COPY requirements.txt ./
 RUN pip install -r requirements.txt --default-timeout=100
+RUN pip install gunicorn
+RUN pip install daphne
 
 COPY . ./
 
--- app/settings.py
+++ app/settings.py
@@ -22,7 +22,9 @@ INSTALLED_APPS = [
     'chat',
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sessions',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
@@ -37,9 +39,11 @@ MIDDLEWARE = [
     'silk.middleware.SilkyMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
