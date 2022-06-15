7ba31e763b753636e94ca558e0c28feee0d5b2f2
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2022-06-15 10:44:39 +0300

    config to new ip

--- frontend/src/services/server.js
+++ frontend/src/services/server.js
@@ -1,6 +1,6 @@
 export default class Server {
    constructor() {
-      this._apiBase = 'http://146.190.230.146:8000/api'
+      this._apiBase = 'http://142.93.235.128:8000/api'
    }
 
    async postResource(url, data) {
