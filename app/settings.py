57ee4d1ea1d368ad40d9c1e91ea9d012d2d6a450
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2022-06-21 23:48:17 +0300

    connect to server db

--- app/settings.py
+++ app/settings.py
@@ -76,6 +76,7 @@ if not local_settings:
             'NAME': os.environ.get('DB_NAME'),
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASS'),
+            'PORT': os.environ.get('DB_PORT')
         }
     }
 
