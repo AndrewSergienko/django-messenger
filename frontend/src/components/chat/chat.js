4b995af77bf3a4896004510d8524e70972c92a75
Andrey Slynko <qkston22@gmail.com>
2022-06-30 22:25:35 +0300

    can change first name and last name in your profile

--- frontend/src/components/chat/chat.js
+++ frontend/src/components/chat/chat.js
@@ -1,214 +1,219 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { ToastContainer, toast } from 'react-toastify';
-import Server from '../../services/server';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import ChatSideBar from '../chatSideBar/chatSideBar';
-import Messages from '../messages';
+import Server from "../../services/server";
 
-import LogoutImage from '../../assets/log-out.png';
+import ChatSideBar from "../chatSideBar/chatSideBar";
+import Messages from "../messages";
 
-import 'react-toastify/dist/ReactToastify.css';
+import LogoutImage from "../../assets/log-out.png";
+
 export default class Chat extends Component {
-   server = new Server(); 
-   chatSocket = new WebSocket(`ws://localhost:8000/ws/chat/${this.props.authToken}/`);
+	server = new Server();
+	chatSocket = new WebSocket(`ws://localhost:8000/ws/chat/${this.props.authToken}/`);
 
-   state = {
-      chats: [],
-      activeChat: 0,
-      messages: [],
-      me: {
-         id: '',
-         first_name: '',
-         last_name: ''
-      },
-      friend: {
-         id: '',
-         first_name: '',
-         last_name: ''
-      }
-   }
+	state = {
+		chats: [],
+		activeChat: 0,
+		messages: [],
+		me: {},
+		friend: {},
+	};
 
-   componentDidMount() {
-      const {chatList, authToken} = this.props;
-      chatList(authToken).then(response => {
-         this.setState({chats: response});
-      })
-   }
+	componentDidMount() {
+		const { chatList, authToken } = this.props;
+		this.userInfo("me");
+		chatList(authToken).then(response => {
+			this.setState({ chats: response });
+		});
+	}
 
-   chatMessages = async (chatId, messagesCount, id, first_name, last_name) => {
-      const result = await this.server.getChatMessages(this.props.authToken, chatId, messagesCount);
-      this.setState({
-         messages: result,
-         friend: {
-            id,
-            first_name,
-            last_name: last_name ? last_name : ''
-         }
-      })
-   }
+	chatMessages = async (chatId, messagesCount) => {
+		const result = await this.server.getChatMessages(this.props.authToken, chatId, messagesCount);
+		this.setState({ messages: result.reverse() });
+	};
 
-   addNewMessageToChat = (chatId, text, senderId) => {
-      this.setState({
-         messages: [...this.state.messages, {chat: chatId, date: new Date(), read: [], text, user: senderId}]
-      })
-   }
+	addNewMessageToChat = (chatId, text, senderId) => {
+		this.setState({
+			messages: [...this.state.messages, { chat: chatId, date: new Date(), read: [], text, user: senderId }],
+		});
+	};
 
-   addNewMessageToSideBar = (data) => {
-      if (data.type === 'message') {
-         this.setState({
-            chats: this.state.chats.map(chat => {
-               if (chat.id === data.message.chat) {
-                  return {...chat, last_message: {text: data.message.text, date: data.message.date}}
-               } else {
-                  return chat
-               }
-            })
-         })
-      } else {
-         this.setState({
-            chats: this.state.chats.map(chat => {
-               if (chat.id === data.chatId) {
-                  return {...chat, last_message: {text: data.text, date: data.date}}
-               } else {
-                  return chat
-               }
-            })
-         })
-      }
-   }
+	addNewMessageToSideBar = data => {
+		if (data.type === "message") {
+			this.setState({
+				chats: this.state.chats.map(chat => {
+					if (chat.id === data.message.chat) {
+						return { ...chat, last_message: { text: data.message.text, date: data.message.date } };
+					} else {
+						return chat;
+					}
+				}),
+			});
+		} else {
+			this.setState({
+				chats: this.state.chats.map(chat => {
+					if (chat.id === data.chatId) {
+						return { ...chat, last_message: { text: data.text, date: data.date } };
+					} else {
+						return chat;
+					}
+				}),
+			});
+		}
+	};
 
-   setActiveChat = (chatId) => {
-      this.setState({activeChat: chatId})
-   }
+	setActiveChat = chatId => {
+		this.setState({ activeChat: chatId });
+	};
 
-   userInfo = async () => {
-      const result = await this.server.getUserInfo(this.props.authToken);
-      this.setState({
-         me: {
-            id: result.id,
-            first_name: result.first_name,
-            last_name: result.last_name ? result.last_name : ''
-         }
-      })
-   }
+	userInfo = async id => {
+		const result = await this.server.getUserInfo(this.props.authToken, id);
+		if (id === "me") {
+			this.setState({ me: result });
+		} else {
+			this.setState({ friend: result });
+		}
+	};
 
-   logout = () => {
-      localStorage.removeItem('authKey');
-      window.location.reload();
-   }
+	logout = () => {
+		localStorage.removeItem("authKey");
+		window.location.reload();
+	};
 
-   notify = (data) => data.message && toast.info(
-      <>
-         <NotifyTitle>{data.message.user.first_name} {data.message.user.last_name}</NotifyTitle>
-         <NotifyText>{data.message.text}</NotifyText>
-      </>, {
-      position: "top-right",
-      autoClose: 5000,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-   });
+	messageNotify = data =>
+		data.message &&
+		toast.info(
+			<>
+				<NotifyTitle>
+					{data.message.user.first_name} {data.message.user.last_name}
+				</NotifyTitle>
+				<NotifyText>{data.message.text}</NotifyText>
+			</>,
+			{
+				position: "top-right",
+				autoClose: 5000,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+			}
+		);
 
-   render() {
-      this.chatSocket.onmessage = (event) => {
-         const data = JSON.parse(event.data);
-         data && this.notify(data) && this.addNewMessageToSideBar(data);
-         if (data.message && data.message.chat === this.state.activeChat) {
-            this.addNewMessageToChat(data.message.chat, data.message.text, data.message.user.id);
-         }
-      }
-      
-      return (
-         <>
-            <Header>
-               <LogoutBtn onClick={this.logout}>
-                  <img src={LogoutImage} alt='Logout' />
-               </LogoutBtn>
-            </Header>
-            <ChatSideBar 
-               chats={this.state.chats}
-               userInfo={this.userInfo}
-               chatMessages={this.chatMessages}
-               setActiveChat={this.setActiveChat}/>
-            { this.state.activeChat ? <Messages 
-               authToken={this.props.authToken} 
-               messages={this.state.messages} 
-               me={this.state.me} 
-               friend={this.state.friend} 
-               activeChat={this.state.activeChat}
-               addNewMessageToChat={this.addNewMessageToChat}
-               addNewMessageToSideBar={this.addNewMessageToSideBar}
-               notify={this.notify}
-               chatSocket={this.chatSocket}/> : <ChooseChat>Select a chat to start a messaging</ChooseChat>}
-            <StyledNotify
-               position="top-right"
-               autoClose={5000}
-               newestOnTop
-               closeOnClick
-               rtl={false}
-               pauseOnFocusLoss
-               draggable
-               pauseOnHover
-            />
-         </>
-      )
-   }
+	chatCreatedNotify = data =>
+		data.message &&
+		toast.info(
+			<NotifyTitle>
+				{data.message.user.first_name} {data.message.user.last_name} was created chat with you.
+			</NotifyTitle>,
+			{
+				position: "top-right",
+				autoClose: 5000,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+			}
+		);
+
+	changeUserInfo = async (first_name, last_name) => {
+		const result = await this.server.changeUserProfile(first_name, last_name, this.props.authToken);
+		this.setState({ me: result });
+	};
+
+	render() {
+		this.chatSocket.onmessage = event => {
+			const data = JSON.parse(event.data);
+			data && this.messageNotify(data) && this.addNewMessageToSideBar(data);
+			if (data.message && data.message.chat === this.state.activeChat) {
+				this.addNewMessageToChat(data.message.chat, data.message.text, data.message.user.id);
+			}
+		};
+
+		return (
+			<>
+				<Header>
+					<LogoutBtn onClick={this.logout}>
+						<img src={LogoutImage} alt="Logout" />
+					</LogoutBtn>
+				</Header>
+				<ChatSideBar chats={this.state.chats} userInfo={this.userInfo} chatMessages={this.chatMessages} setActiveChat={this.setActiveChat} />
+				{this.state.activeChat ? (
+					<Messages
+						authToken={this.props.authToken}
+						messages={this.state.messages}
+						me={this.state.me}
+						friend={this.state.friend}
+						activeChat={this.state.activeChat}
+						addNewMessageToChat={this.addNewMessageToChat}
+						addNewMessageToSideBar={this.addNewMessageToSideBar}
+						notify={this.chatCreatedNotify}
+						chatSocket={this.chatSocket}
+						changeUserInfo={this.changeUserInfo}
+					/>
+				) : (
+					<ChooseChat>Select a chat to start a messaging</ChooseChat>
+				)}
+				<StyledNotify position="top-right" autoClose={5000} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+			</>
+		);
+	}
 }
 
 const Header = styled.section`
-   position: fixed;
-   width: 100%;
-   background: #fff;
-   border-bottom: 1px solid #cacaca;
-   text-align: end;
-`
+	position: fixed;
+	width: 100%;
+	background: #fff;
+	border-bottom: 1px solid #cacaca;
+	text-align: end;
+`;
 
 const LogoutBtn = styled.button`
-   margin: 20px;
-   background: transparent;
-   border: none;
-   transition: 0.2s;
+	margin: 20px;
+	background: transparent;
+	border: none;
+	transition: 0.2s;
 
-   &:hover {
-      transform: scale(110%);
-   }
-`
+	&:hover {
+		transform: scale(110%);
+	}
+`;
 
 const ChooseChat = styled.p`
-   position: absolute;
-   top: 50%;
-   width: 100%;
-   padding-left: 27%;
+	position: absolute;
+	top: 50%;
+	width: 100%;
+	padding-left: 27%;
 
-   font-size: 20px;
-   text-align: center;
-`
+	font-size: 20px;
+	text-align: center;
+`;
 
 const StyledNotify = styled(ToastContainer)`
-   .Toastify__progress-bar {
-      height: 2px;
-      background: #0D6EFD;
-   }
-   .Toastify__toast-icon {
-      display: none;
-   }
-`
+	.Toastify__progress-bar {
+		height: 2px;
+		background: #0d6efd;
+	}
+	.Toastify__toast-icon {
+		display: none;
+	}
+`;
 
 const NotifyTitle = styled.h4`
-   width: 290px;
-   margin: 0;
-   color: #000;
-   font-size: 16px;
-   font-weight: bold;
-`
+	width: 290px;
+	margin: 0;
+	color: #000;
+	font-size: 16px;
+	font-weight: bold;
+`;
 
 const NotifyText = styled.p`
-   width: 290px;
-   margin: 10px 0 0 0;
-   font-size: 14px;
-   overflow: hidden;
-   white-space: nowrap;
-   text-overflow: ellipsis;
-`
+	width: 290px;
+	margin: 10px 0 0 0;
+	font-size: 14px;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+`;
--- frontend/src/services/server.js
+++ frontend/src/services/server.js
@@ -1,70 +1,74 @@
 export default class Server {
-   constructor() {
-      this._apiBase = 'http://142.93.235.128:8000/api'
-   }
+	constructor() {
+		this._apiBase = "http://127.0.0.1:8000/api";
+	}
 
-   async postResource(url, data) {
-      return await fetch(`${this._apiBase}${url}`, {
-         method: 'POST',
-         body: JSON.stringify(data),
-         headers: {
-            'Content-Type': 'application/json'
-         }
-      })
-   };
+	async postResource(url, data, token) {
+		return await fetch(`${this._apiBase}${url}`, {
+			method: "POST",
+			body: JSON.stringify(data),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: token ? `Token ${token}` : "",
+			},
+		});
+	}
 
-   async getResource(url, headers) {
-      return await fetch(`${this._apiBase}${url}`, {headers: headers});
-   };
+	async getResource(url, headers) {
+		return await fetch(`${this._apiBase}${url}`, { headers: headers });
+	}
 
-   // Get token for login
-   loginUser = (email, password) => {
-      const data = {email, password};
-      return this
-               .postResource('/auth/', data)
-               .then(errors => errors.json());
-   }
+	// Get token for login
+	loginUser = (email, password) => {
+		const data = { email, password };
+		return this.postResource("/auth/", data).then(errors => errors.json());
+	};
 
-   // Registration user
-   createToken = (email) => {
-      return this
-               .postResource('/users/create_token', {email})
-               .then(errors => errors.json())
-               .catch(() => {return});
-   }
+	// Registration user
+	createToken = email => {
+		return this.postResource("/users/create_token", { email })
+			.then(errors => errors.json())
+			.catch(() => {
+				return;
+			});
+	};
 
-   verifyToken = (email, token) => {
-      const data = {email, token};
-      return this
-               .postResource('/users/verify_token', data)
-               .then(errors => errors)
-               .catch(() => {return});
-   }
+	verifyToken = (email, token) => {
+		const data = { email, token };
+		return this.postResource("/users/verify_token", data)
+			.then(errors => errors)
+			.catch(() => {
+				return;
+			});
+	};
 
-   registrationUser = (email, username, password, first_name, last_name) => {
-      const data = {email, username, password, first_name, last_name};
-      return this
-               .postResource('/users/', data)
-               .then(errors => errors.json())
-               .catch(() => {return});
-   }
+	registrationUser = (email, username, password, first_name, last_name) => {
+		const data = { email, username, password, first_name, last_name };
+		return this.postResource("/users/", data)
+			.then(errors => errors.json())
+			.catch(() => {
+				return;
+			});
+	};
 
-   // Chat
-   getChatsList = (token) => {
-      return this
-               .getResource(`/chats/list?token=${token}`, {'Authorization': `Token ${token}`})
-               .then(chats => chats.json())
-   }
+	// Chat
+	getChatsList = token => {
+		return this.getResource(`/chats/list?token=${token}`, { Authorization: `Token ${token}` }).then(chats => chats.json());
+	};
 
-   getChatMessages = (token, chatId, messagesCount = 25) => {
-      return this
-         .getResource(`/chats/${chatId}/messages?messages_num=${messagesCount}`, {'Authorization': `Token ${token}`})
-         .then(chats => chats.json())
-   }
+	getChatMessages = (token, chatId, messagesCount = 25) => {
+		return this.getResource(`/chats/${chatId}/messages?messages_num=${messagesCount}`, { Authorization: `Token ${token}` }).then(chats =>
+			chats.json()
+		);
+	};
 
-   getUserInfo = (token, id) => {
-      return this
-         .getResource(`/users/${id}`, {'Authorization': `Token ${token}`})
-         .then(chats => chats.json())
-   }
+	getUserInfo = (token, id) => {
+		return this.getResource(`/users/${id}`, { Authorization: `Token ${token}` }).then(chats => chats.json());
+	};
+
+	// Profile
+	changeUserProfile = (first_name, last_name, token) => {
+		const data = { first_name, last_name };
+		return this.postResource("/users/me/edit", data, token).then(user => user.json());
+	};
 }
