b4821952fb889289f5aec2aec44cde40ad3f56d5
Andrey Slynko <qkston22@gmail.com>
2022-06-30 22:25:35 +0300

    can change first name and last name in your profile

--- frontend/src/components/messages/messages.js
+++ frontend/src/components/messages/messages.js
@@ -1,159 +1,192 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Modal from 'react-modal';
+import React, { Component } from "react";
+import styled from "styled-components";
+import Modal from "react-modal";
 
-import defaultAvatar from '../../assets/default-avatar.png';
-import sendButton from '../../assets/send-button.png';
+import defaultAvatar from "../../assets/default-avatar.png";
+import sendButton from "../../assets/send-button.png";
 
-import EmptyChat from '../emptyChat';
-import Profile from '../profile/profile';
+import EmptyChat from "../emptyChat";
+import Profile from "../profile/profile";
 
 export default class Messages extends Component {
-   state = {
-      message: '',
-      modalIsOpen: false,
-      userId: 0
-   } 
+	state = {
+		message: "",
+		modalIsOpen: false,
+		userId: 0,
+	};
 
-   componentDidMount() {
-      Modal.setAppElement('body');
-   }
+	componentDidMount() {
+		Modal.setAppElement("body");
+	}
 
-   componentDidUpdate() {
-      window.scroll({
-         top: document.body.offsetHeight,
-         left: 0, 
-         behavior: 'smooth',
-      }); 
-   }
+	componentDidUpdate() {
+		window.scroll({
+			top: document.body.offsetHeight,
+			left: 0,
+			behavior: "smooth",
+		});
+	}
 
-   onSubmit(event,  chatId) {
-      const { addNewMessageToChat, addNewMessageToSideBar, me } = this.props;
-      event.preventDefault();
-      
-      this.props.chatSocket.send(
-         JSON.stringify({
-            'type': 'message',
-            'chat': chatId,
-            'text': this.state.message
-         })
-      );
-      
-      addNewMessageToChat(chatId, this.state.message, me.id);
-      addNewMessageToSideBar({chatId: chatId, text: this.state.message, date: new Date()});
-      this.setState({message: ''})
-   }
+	onSubmit(event, chatId) {
+		const { addNewMessageToChat, addNewMessageToSideBar, me } = this.props;
+		event.preventDefault();
 
-   openModal = () => {
-      this.setState({modalIsOpen: true})
-   }
+		this.props.chatSocket.send(
+			JSON.stringify({
+				type: "message",
+				chat: chatId,
+				text: this.state.message,
+			})
+		);
 
-   closeModal = () => {
-      this.setState({modalIsOpen: false})
-   }
+		addNewMessageToChat(chatId, this.state.message, me.id);
+		addNewMessageToSideBar({ chatId: chatId, text: this.state.message, date: new Date() });
+		this.setState({ message: "" });
+	}
 
-   render() {
-      const { activeChat, messages, me, friend } = this.props;
+	openModal = () => {
+		this.setState({ modalIsOpen: true });
+	};
 
-      return (
-         <Wrap>
-            {messages.length ? messages.map((message, index) => {
-               return <Message key={index}>
-                  <Avatar src={defaultAvatar} alt='Avatar' onClick={() => {this.openModal(); this.setState({userId: message.user});}}/>
-                  <section>
-                     <Username>{friend.id === message.user ? `${friend.first_name} ${friend.last_name}` : `${me.first_name} ${me.last_name}`}</Username>
-                     <Time>{new Date(message.date).toLocaleString('en-US', {month: 'long', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false})}</Time>
-                     <section>
-                        <MessageText>{message.text}</MessageText>
-                     </section>
-                  </section>
-               </Message>
-            }) : <EmptyChat/> }
-            {activeChat ? <form onSubmit={(event) => this.onSubmit(event, activeChat)}>
-               <Input 
-                  type='text' 
-                  placeholder='Enter your message...' 
-                  value={this.state.message}
-                  onChange={(e) => this.setState({message: e.target.value})}
-                  required/>
-               <Send type='submit'>
-                  <img src={sendButton} alt='Send'/>
-               </Send>
-            </form> : ''}
-            <Profile isOpen={this.state.modalIsOpen} info={this.state.userId === me.id ? me : friend} closeModal={this.closeModal}/>
-         </Wrap>
-      )
-   }
+	closeModal = () => {
+		this.setState({ modalIsOpen: false });
+	};
+
+	render() {
+		const { activeChat, messages, me, friend, changeUserInfo } = this.props;
+
+		return (
+			<Wrap>
+				{messages.length ? (
+					messages.map((message, index) => {
+						return (
+							<Message key={index}>
+								<Avatar
+									src={defaultAvatar}
+									alt="Avatar"
+									onClick={() => {
+										this.openModal();
+										this.setState({ userId: message.user });
+									}}
+								/>
+								<section>
+									<Username>
+										{friend.id === message.user ? `${friend.first_name} ${friend.last_name}` : `${me.first_name} ${me.last_name}`}
+									</Username>
+									<Time>
+										{new Date(message.date).toLocaleString("en-US", {
+											month: "long",
+											day: "2-digit",
+											hour: "2-digit",
+											minute: "2-digit",
+											hour12: false,
+										})}
+									</Time>
+									<section>
+										<MessageText>{message.text}</MessageText>
+									</section>
+								</section>
+							</Message>
+						);
+					})
+				) : (
+					<EmptyChat />
+				)}
+				{activeChat ? (
+					<form onSubmit={event => this.onSubmit(event, activeChat)}>
+						<Input
+							type="text"
+							placeholder="Enter your message..."
+							value={this.state.message}
+							onChange={e => this.setState({ message: e.target.value })}
+							required
+						/>
+						<Send type="submit">
+							<img src={sendButton} alt="Send" />
+						</Send>
+					</form>
+				) : (
+					""
+				)}
+				<Profile
+					userId={me.id}
+					isOpen={this.state.modalIsOpen}
+					info={this.state.userId === me.id ? me : friend}
+					closeModal={this.closeModal}
+					changeUserInfo={changeUserInfo}
+				/>
+			</Wrap>
+		);
+	}
 }
-
 
 // Styled components
 const Wrap = styled.section`
-   padding-top: 3.7%;
-   margin-left: 27%;
-   margin-bottom: 75px;
-`
+	padding-top: 3.7%;
+	margin-left: 27%;
+	margin-bottom: 75px;
+`;
 
 const Message = styled.section`
-   display: flex;
-   margin-right: 30px;
-   padding: 25px 0;
-   border-top: 1px solid #cacaca;
+	display: flex;
+	margin-right: 30px;
+	padding: 25px 0;
+	border-top: 1px solid #cacaca;
 
-   &:first-child {
-      border-top: none;
-   }
+	&:first-child {
+		border-top: none;
+	}
 `;
 
 const Avatar = styled.img`
-   width: 48px;
-   height: 48px;
-   margin-right: 20px;
-   border-radius: 50%;
-   cursor: pointer;
-`
+	width: 48px;
+	height: 48px;
+	margin-right: 20px;
+	border-radius: 50%;
+	cursor: pointer;
+`;
 
 const Username = styled.span`
-   margin-right: 15px;
-   font-size: 16px;
-   font-weight: bold;
-`
+	margin-right: 15px;
+	font-size: 16px;
+	font-weight: bold;
+`;
 
 const MessageText = styled.span`
-   width: 100%;
-   font-size: 14px;
-`
+	width: 100%;
+	font-size: 14px;
+`;
 
 const Time = styled.span`
-   font-size: 12px;
-`
+	font-size: 12px;
+`;
 
 const Input = styled.input`
-   position: fixed;
-   bottom: 0;
-   width: 69%;
-   margin: 20px 0;
-   padding: 10px;
+	position: fixed;
+	bottom: 0;
+	width: 69%;
+	margin: 20px 0;
+	padding: 10px;
 
-   border-radius: 5px;
-   border: 1px solid #cacaca;
+	border-radius: 5px;
+	border: 1px solid #cacaca;
 
-   &:focus {
-      box-shadow: none !important;
-      outline: none !important;
-   }
+	&:focus {
+		box-shadow: none !important;
+		outline: none !important;
+	}
 `;
 
 const Send = styled.button`
-   position: fixed;
-   bottom: 0;
-   right: 0;
-   margin: 20px 20px 25px 20px;
-   background: none;
-   border: none;
-   transition: 0.2s;
+	position: fixed;
+	bottom: 0;
+	right: 0;
+	margin: 20px 20px 25px 20px;
+	background: none;
+	border: none;
+	transition: 0.2s;
 
-   &:hover {
-      transform: scale(110%);
-   }
+	&:hover {
+		transform: scale(110%);
+	}
 `;
--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,109 +1,226 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import Modal from 'react-modal';
+import React, { Component } from "react";
+import styled from "styled-components";
+import Modal from "react-modal";
 
-import defaultAvatar from '../../assets/default-avatar.png';
+import defaultAvatar from "../../assets/default-avatar.png";
+import editBtn from "../../assets/edit.png";
 
 export default class Profile extends Component {
-   render() {
-      const {isOpen, info, closeModal} = this.props;
+	state = {
+		changeMode: false,
+		first_name: "",
+		last_name: "",
+	};
 
-      isOpen ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'unset';
+	switchMode = () => {
+		this.setState({ changeMode: !this.state.changeMode });
+	};
 
-      return (
-         <StyledModal 
-            isOpen={isOpen}
-            onRequestClose={closeModal}>
-            <Title>User info</Title>
-            <Upper>
-               <Avatar src={info.avatar === null ? defaultAvatar : ''} alt='Avatar' />
-               <Info>
-                  <FullName>{info.first_name} {info.last_name}</FullName>
-                  <Status>{info.active_status}</Status>
-               </Info>
-            </Upper>
-            <Bottom>
-               {info.phone ? <Label>Phone: <Link href={`tel:${info.phone}`}>{info.phone}</Link></Label> : ''}
-               <Label>Email: <Link href={`mailto:${info.email}`}>{info.email}</Link></Label>
-               <Label>Username: <Link>@{info.username}</Link></Label>
-            </Bottom>
-         </StyledModal>
-      )
-   }
+	changeInput = event => {
+		const target = event.target;
+
+		if (target.id === "first-name-input") {
+			this.setState({
+				first_name: target.value,
+			});
+		} else if (target.id === "last-name-input") {
+			this.setState({
+				last_name: target.value,
+			});
+		}
+	};
+
+	submitForm = async event => {
+		event.preventDefault();
+		const { first_name, last_name } = this.state;
+		const { changeUserInfo } = this.props;
+
+		await changeUserInfo(first_name, last_name);
+		this.setState({
+			changeMode: !this.state.changeMode,
+			first_name: "",
+			last_name: "",
+		});
+	};
+
+	render() {
+		const { userId, isOpen, info, closeModal } = this.props;
+
+		isOpen ? (document.body.style.overflow = "hidden") : (document.body.style.overflow = "unset");
+
+		return (
+			<StyledModal
+				isOpen={isOpen}
+				onRequestClose={() => {
+					closeModal();
+					this.setState({ changeMode: false });
+				}}>
+				<Title>User info</Title>
+				<Upper>
+					<Avatar src={info.avatar === null ? defaultAvatar : ""} alt="Avatar" />
+					<Info>
+						{!this.state.changeMode ? (
+							<FullName>
+								{info.first_name} {info.last_name}
+								{userId === info.id ? <Edit src={editBtn} alt="Edit" onClick={this.switchMode} /> : ""}
+							</FullName>
+						) : (
+							<ChangeMode onSubmit={event => this.submitForm(event)}>
+								<Input
+									id="first-name-input"
+									className="form-control"
+									type="text"
+									placeholder="First name"
+									value={this.state.first_name}
+									onChange={this.changeInput}
+									required
+								/>
+								<Input
+									id="last-name-input"
+									className="form-control"
+									type="text"
+									placeholder="Second name"
+									value={this.state.last_name}
+									onChange={this.changeInput}
+								/>
+								<Submit className="btn btn-primary" type="submit">
+									Save
+								</Submit>
+								<Close onClick={this.switchMode}>x</Close>
+							</ChangeMode>
+						)}
+						<Status>{info.active_status ? info.active_status : "online"}</Status>
+					</Info>
+				</Upper>
+				<Bottom>
+					{info.phone ? (
+						<Label>
+							Phone: <Link href={`tel:${info.phone}`}>{info.phone}</Link>
+						</Label>
+					) : (
+						""
+					)}
+					<Label>
+						Email: <Link href={`mailto:${info.email}`}>{info.email}</Link>
+					</Label>
+					<Label>
+						Username: <Link>@{info.username}</Link>
+					</Label>
+				</Bottom>
+			</StyledModal>
+		);
+	}
 }
-
 
 // Styled components
 const StyledModal = styled(Modal)`
-   width: 500px;
-   height: 400px;
+	width: 500px;
+	height: 400px;
 
-   padding: 20px;
+	padding: 20px;
 
-   position: absolute;
-   top: 50%;
-   left: 50%;
-   transform: translate(-50%, -50%);
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
 
-   background: #fff;
-   box-shadow: 0px 0px 15px 1px #000;
-   border-radius: 10px;
+	background: #fff;
+	box-shadow: 0px 0px 15px 1px #000;
+	border-radius: 10px;
 
-   &:focus {
-      outline: none;
-   }
+	&:focus {
+		outline: none;
+	}
 `;
 
 const Title = styled.h2`
-   margin-bottom: 20px;
-   font-size: 26px;
-   font-weight: bold;
-`
+	margin-bottom: 20px;
+	font-size: 26px;
+	font-weight: bold;
+`;
 
 const Upper = styled.section`
-   display: flex;
-   align-items: center;
-   margin-bottom: 40px;
-`
+	display: flex;
+	align-items: center;
+	margin-bottom: 40px;
+`;
 
 const Avatar = styled.img`
-   margin-right: 20px;
-   width: 64px;
-   height: 64px;
-   border-radius: 100px;
-`
+	margin-right: 20px;
+	width: 64px;
+	height: 64px;
+	border-radius: 100px;
+`;
 
 const Info = styled.section`
-   display: flex;
-   flex-direction: column;
-`
+	display: flex;
+	flex-direction: column;
+`;
 
 const FullName = styled.p`
-   margin: 0;
-   font-size: 18px;
-   font-weight: bold;
-`
+	margin: 0;
+	font-size: 18px;
+	font-weight: bold;
+`;
 
 const Status = styled.span`
-   font-size: 14px;
-`
+	font-size: 14px;
+`;
 
 const Bottom = styled.section`
-   display: flex;
-   flex-direction: column;
-`
+	display: flex;
+	flex-direction: column;
+`;
 
 const Label = styled.span`
-   margin-bottom: 10px;
-`
+	margin-bottom: 10px;
+`;
 
 const Link = styled.a`
-   color: #000;
-   text-decoration: none;
-   cursor: pointer;
-   
-   &:hover {
-      color: #000;
-      text-decoration: underline;
-   }
-`
+	color: #000;
+	text-decoration: none;
+	cursor: pointer;
+
+	&:hover {
+		color: #000;
+		text-decoration: underline;
+	}
+`;
+
+const Edit = styled.img`
+	margin-bottom: 5px;
+	margin-left: 5px;
+	cursor: pointer;
+`;
+
+const ChangeMode = styled.form`
+	display: flex;
+	margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+	margin-right: 5px;
+	width: 35%;
+	height: 30px;
+	font-size: 14px;
+
+	&:focus {
+		box-shadow: none !important;
+	}
+`;
+
+const Submit = styled.button`
+	height: 30px;
+	font-size: 14px;
+	box-shadow: 0px 15px 50px -15px #0d6efd;
+
+	&:focus {
+		box-shadow: 0px 15px 50px -15px #0d6efd;
+	}
+`;
+
+const Close = styled.button`
+	margin-left: 5px;
+	background: none;
+	border: none;
+`;
